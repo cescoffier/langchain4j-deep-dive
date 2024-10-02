ce09c461a0b75361293aa921c0669c41d1305063
Eric Deandrea <edeandrea@redhat.com>
2024-10-02 08:30:51 -0400

    Fixing cohere

--- 4-rag/src/main/java/dev/langchain4j/quarkus/deepdive/RagRetriever.java
+++ 4-rag/src/main/java/dev/langchain4j/quarkus/deepdive/RagRetriever.java
@@ -10,6 +10,7 @@ import dev.langchain4j.model.scoring.ScoringModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.aggregator.ContentAggregator;
+import dev.langchain4j.rag.content.aggregator.ReRankingContentAggregator;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.rag.content.retriever.WebSearchContentRetriever;
@@ -72,13 +73,13 @@ public class RagRetriever {
     }
 
     private ContentAggregator getContentAggregator(ScoringModel scoringModel) {
-      return null;
+//      return null;
 
-//      return ReRankingContentAggregator.builder()
-//          .scoringModel(scoringModel)
-//          .querySelector(ReRankingContentAggregator.DEFAULT_QUERY_SELECTOR)
-//          .minScore(0.6)
-//          .build();
+      return ReRankingContentAggregator.builder()
+          .scoringModel(scoringModel)
+          .querySelector(ReRankingContentAggregator.DEFAULT_QUERY_SELECTOR)
+          .minScore(0.6)
+          .build();
 
 //      return new CustomReRankingContentAggregator(scoringModel, 0.6);
     }
