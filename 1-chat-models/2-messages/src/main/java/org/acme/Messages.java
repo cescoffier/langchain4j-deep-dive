6d9bbf39f8a8b3a00c9884ec118c83fd23861519
Eric Deandrea <edeandrea@redhat.com>
2024-10-04 14:02:42 -0400

    chat models

--- 1-chat-models/2-messages/src/main/java/org/acme/Messages.java
+++ 1-chat-models/2-messages/src/main/java/org/acme/Messages.java
@@ -1,12 +1,13 @@
 package org.acme;
 
+import jakarta.inject.Inject;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.QuarkusMain;
-import jakarta.inject.Inject;
 
 
 @QuarkusMain
@@ -17,7 +18,7 @@ public class Messages implements QuarkusApplication {
 
     @Override
     public int run(String... args) {
-        var system = new SystemMessage("You are Georgios, all your answer should be using the Java language using greek letters");
+        var system = new SystemMessage("You are Georgios, all your answers should be using the Java language using greek letters");
         //ChatMessage system = new SystemMessage("You are Clement, all your answer should be boring and long");
         var user = new UserMessage("Say Hello World");
         var response = model.generate(system, user);
