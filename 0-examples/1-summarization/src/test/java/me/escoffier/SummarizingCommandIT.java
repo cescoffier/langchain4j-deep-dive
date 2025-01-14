476250fe2c3542b0edca76c30ffc137b429d0ecb
Eric Deandrea <edeandrea@redhat.com>
2025-01-14 16:05:23 -0500

    Adding some tests

--- 0-examples/1-summarization/src/test/java/me/escoffier/SummarizingCommandIT.java
+++
@@ -1,7 +0,0 @@
-package me.escoffier;
-
-import io.quarkus.test.junit.main.QuarkusMainIntegrationTest;
-
-@QuarkusMainIntegrationTest
-public class SummarizingCommandIT extends SummarizingCommandTests {
-}
